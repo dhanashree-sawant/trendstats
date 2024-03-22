3e4368c7f6800b27f78b56f8f210d0b956e80f63
Dhanashree Sawant <87513216+dhanashree-sawant@users.noreply.github.com>
2024-03-22 17:16:42 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Navigation from "@/components/navigation";
+
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <Navigation/>
         <Main />
         <NextScript />
       </body>
